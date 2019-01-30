bcd4905478ab5d86181f65be7eb97a606346f2a1
novikdm <novik.dmitro@gmail.com>
2019-01-30 22:40:43 +0200

    add function of translate balancing

--- src/main/java/MainPage.java
+++ src/main/java/MainPage.java
@@ -1,15 +1,12 @@
 import org.json.JSONArray;
 import org.json.JSONObject;
-import org.json.JSONWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.BufferedWriter;
 import java.io.IOException;
-import java.io.Writer;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
